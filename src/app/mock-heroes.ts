b95e19bdcf5f6def22c2569567f679fcbecc3ac8
Abdeslam Loukili <abdeslam.edu@gmail.com>
2022-08-20 22:02:54 +0100

    added all heroes and one hero details' components

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+// creating 10 heroes and exporting them as a constant
+
+export const HEROES: Hero[] = [
+    {id: 12, name: "someone"},
+    {id: 13, name: "sometwo"},
+    {id: 14, name: "somethree"},
+    {id: 15, name: "somefour"},
+    {id: 16, name: "somefive"},
+    {id: 17, name: "somesex"},
+    {id: 18, name: "someseven"},
+    {id: 19, name: "someeight"},
+]
